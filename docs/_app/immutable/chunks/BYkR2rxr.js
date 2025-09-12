415a180d4df551dc80e63d08e26055134954033b
pigeonwatcher <benjamincurry777@gmail.com>
2025-09-12 16:20:28 +0100

    chore: static site setup

---
+++ docs/_app/immutable/chunks/BYkR2rxr.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1j6b8r8?.base??"",a=globalThis.__sveltekit_1j6b8r8?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/CRtfuDcu.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_vc1sfv?.base??"",a=globalThis.__sveltekit_vc1sfv?.assets??s;export{a,s as b};
