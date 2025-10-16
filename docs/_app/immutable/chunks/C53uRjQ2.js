54d1e575b04cf39706363229bea4eab8e2d0102b
pigeonwatcher <benjamincurry777@gmail.com>
2025-10-16 12:19:13 +0100

    chore: static site update

--- docs/_app/immutable/chunks/C53uRjQ2.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_16v9epf?.base??"",e=globalThis.__sveltekit_16v9epf?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/DzZPqaDp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1bnhtip?.base??"",t=globalThis.__sveltekit_1bnhtip?.assets??s;export{t as a,s as b};
