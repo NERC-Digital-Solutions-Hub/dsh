7dedeb2485c4d8d79c38daf93350b376753d43c0
pigeonwatcher <benjamincurry777@gmail.com>
2025-10-22 10:32:52 +0100

    refactor: updated labs page to include additional videos

--- Hub/src/routes/labs/+page.svelte
+++ Hub/src/routes/labs/+page.svelte
@@ -1,8 +1,39 @@
 <h1>Labs</h1>
 <p>This is the Labs page.</p>
-<video controls width="640" height="360">
+<!-- <video controls width="640" height="360">
 	<source
 		src="https://github.com/NERC-Digital-Solutions-Hub/dsh-content/raw/refs/heads/main/videos/llm/func-call/llm-func-call-poc.mp4"
+		type="video/mp4"
+	/>
+	Your browser does not support the video tag.
+</video> -->
+<video controls width="640" height="360">
+	<source
+		src="https://github.com/NERC-Digital-Solutions-Hub/dsh-content-videos/raw/refs/heads/dev/videos/llm/func-call/llm-func-call-poc-filter-by-area.mp4"
+		type="video/mp4"
+	/>
+	Your browser does not support the video tag.
+</video>
+
+<video controls width="640" height="360">
+	<source
+		src="https://github.com/NERC-Digital-Solutions-Hub/dsh-content-videos/raw/refs/heads/dev/videos/llm/func-call/llm-func-call-poc-pollutants-filter-download.mp4"
+		type="video/mp4"
+	/>
+	Your browser does not support the video tag.
+</video>
+
+<video controls width="640" height="360">
+	<source
+		src="https://github.com/NERC-Digital-Solutions-Hub/dsh-content-videos/raw/refs/heads/dev/videos/llm/func-call/llm-func-call-poc-pollutants-filter.mp4"
+		type="video/mp4"
+	/>
+	Your browser does not support the video tag.
+</video>
+
+<video controls width="640" height="360">
+	<source
+		src="https://github.com/NERC-Digital-Solutions-Hub/dsh-content-videos/raw/refs/heads/dev/videos/llm/func-call/llm-func-call-poc.mp4"
 		type="video/mp4"
 	/>
 	Your browser does not support the video tag.
