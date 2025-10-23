341f7098a0ece62e9a423e32c8a0c4c21304821f
pigeonwatcher <benjamincurry777@gmail.com>
2025-10-23 18:20:09 +0100

    chore: static site update

--- docs/_app/immutable/chunks/Dmbsya9_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1634arj?.base??"",a=globalThis.__sveltekit_1634arj?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/DoxSkvUs.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1nj9eak?.base??"",a=globalThis.__sveltekit_1nj9eak?.assets??s;export{a,s as b};
