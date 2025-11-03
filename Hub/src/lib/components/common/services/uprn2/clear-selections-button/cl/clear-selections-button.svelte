4a11e44023d66d58b17a6a68fc8f830a83f6eafc
pigeonwatcher <benjamincurry777@gmail.com>
2025-11-03 17:22:23 +0000

    feat: created clear-selections-button component

---
+++ Hub/src/lib/components/common/services/uprn2/clear-selections-button/cl/clear-selections-button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Trash } from 'lucide-svelte';
+
+	type Props = {
+		clear: () => void;
+	};
+
+	const { clear }: Props = $props();
+</script>
+
+<Button onclick={() => clear()} variant="destructive" class="size-7" title="Clear all selections">
+	<Trash />
+</Button>
