90009b6ceb9d9b437b9341b6f0efb9490b050ed6
pigeonwatcher <benjamincurry777@gmail.com>
2026-02-02 16:50:53 +0000

    chore: removed unused types

--- packages/uprn-service/src/lib/types/config.ts
+++ packages/uprn-service/src/lib/types/config.ts
@@ -8,18 +8,6 @@ export type AppsUprnConfig = {
 	maps: PortalItemConfig[];
 };
 
-// export type PortalItemConfig = {
-// 	_name?: string;
-// 	title: string;
-// 	portalUrl?: string | null;
-// 	portalItemId: string;
-// 	proxy?: Proxy | null;
-// 	customRenderers?: string;
-// 	areaTreeview?: TreeviewConfig;
-// 	dataTreeview?: TreeviewConfig;
-// 	selectableLayers?: { _name: string; id: string; nameField: string; codeField: string }[];
-// };
-
 export type PortalItemConfig = {
 	_name?: string;
 	title: string;
--- packages/uprn-service/src/lib/types/treeview.ts
+++ packages/uprn-service/src/lib/types/treeview.ts
@@ -20,74 +20,18 @@ export interface InheritanceGroupConfig {
 	inheritedProperties: string[];
 }
 
-// /**
-//  * Main configuration object for the treeview component.
-//  * Contains all the configuration data needed to set up treeview items and their visibility rules.
-//  */
-// export interface TreeviewConfig {
-// 	/** Array of treeview item configurations. Optional - defaults to empty array if not provided */
-// 	items?: TreeviewNodeConfig[];
-// 	/** Array of visibility group configurations. Optional - defaults to empty array if not provided */
-// 	visibilityGroups?: VisibilityGroupConfig[];
-
-// 	/** Array of inheritance group configurations. Optional - defaults to empty array if not provided */
-// 	inheritanceGroups?: InheritanceGroupConfig[];
-
-// 	/** List of field names to hide from display in the treeview. Optional - defaults to no hidden fields if not provided */
-// 	fieldsToHide?: string[];
-// }
-
 /**
  * Main configuration object for the treeview component.
  * Contains all the configuration data needed to set up treeview items and their visibility rules.
  */
 export interface TreeviewConfig {
-	/** Array of treeview ;ayer configurations. Optional - defaults to empty array if not provided */
+	/** Array of treeview layer configurations. Optional - defaults to empty array if not provided */
 	layers?: TreeviewNodeConfig[];
 	/** Array of visibility group configurations. Optional - defaults to empty array if not provided */
 	visibilityGroups?: VisibilityGroupConfig[];
-
 	/** List of field names to hide from display in the treeview. Optional - defaults to no hidden fields if not provided */
 	fieldsToHide?: string[];
 }
-
-// /**
-//  * Configuration for an individual node in the treeview.
-//  * Defines the properties and behavior of a single treeview node, including visibility rules,
-//  * download capabilities, and dependencies on other node.
-//  */
-// export interface TreeviewNodeConfig {
-// 	/** Optional name for configuration management */
-// 	_name?: string;
-// 	/** Unique identifier for the treeview node */
-// 	id: string;
-// 	/** Type of the treeview node, e.g., 'group-layer' */
-// 	type: TreeviewNodeType;
-// 	/** Whether this node can be downloaded by the user. Optional - defaults to false if not specified */
-// 	isDownloadable?: boolean;
-// 	/** Whether this node is visible on initialisation. Optional - defaults to false if not specified */
-// 	isVisibleOnInit?: boolean;
-
-// 	/** Whether this node should be hidden from the user interface AND have its layer visibility set to false. Optional - defaults to false if not specified */
-// 	isHidden?: boolean;
-
-// 	/** Whether this node can have its layer visibility changed. Optional - defaults to false if not specified */
-// 	disableVisibilityToggle?: boolean;
-
-// 	/** Whether this node is expanded/open on initialisation. Optional - defaults to false if not specified */
-// 	isOpenOnInit?: boolean;
-
-// 	/** Whether fields under this node should be shown in the user interface. Optional - defaults to false if not specified */
-// 	showFields?: boolean;
-// 	/** Array of node IDs that this node depends on for visibility. Optional - no dependencies if not specified */
-// 	visibilityDependencyIds?: string[];
-// 	/** ID of the visibility group this node belongs to. Optional - node not part of any group if not specified */
-// 	visibilityGroupId?: string;
-// 	/** ID of the inheritance group this node belongs to. Optional - node not part of any group if not specified */
-// 	inheritanceGroupId?: string;
-// 	/** Optional ID of a custom converter to use for this node, if applicable */
-// 	customConverterId?: string;
-// }
 
 /**
  * Configuration for an individual node in the treeview.
@@ -127,6 +71,9 @@ export interface TreeviewNodeConfig {
 
 	/** Whether this node is expanded/open on initialisation. Optional - defaults to false if not specified */
 	isOpenOnInit?: boolean;
+
+	/** Order index to determine the position of this node among its siblings. Optional - defaults to undefined if not specified */
+	order?: number;
 
 	/** Whether fields under this node should be shown in the user interface. Optional - defaults to false if not specified */
 	showFields?: boolean;
