d6653410f933134157dd3795e5946f1446ed0d74
pigeonwatcher <benjamincurry777@gmail.com>
2026-02-04 14:13:02 +0000

    refactor: added tab titles for each page

--- hub/src/routes/+layout.svelte
+++ hub/src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/state';
 	import NavBar from '$lib/components/nav-bar/nav-bar.svelte';
 
+	const siteName = 'Digital Solutions Hub';
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<title>
+		{page.data?.title ? `${page.data.title} | ${siteName}` : siteName}
+	</title>
+
+	{#if page.data?.description}
+		<meta name="description" content={page.data.description} />
+	{/if}
+</svelte:head>
 
 <main class="main-content">
 	<NavBar />
