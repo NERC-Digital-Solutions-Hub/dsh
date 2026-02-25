74105b6a3fa9f3b0013db10b0cf754375e698160
pigeonwatcher <benjamincurry777@gmail.com>
2026-02-25 14:14:45 +0000

    fix: updated path to use correct casing

--- packages/uprn-service/src/lib/components/Treeview/GetNodeIcon.ts
+++ packages/uprn-service/src/lib/components/Treeview/GetNodeIcon.ts
@@ -1,5 +1,5 @@
-import FeatureLayerIcon from '$lib/assets/feature-layer-16.svg?raw';
-import TileLayerIcon from '$lib/assets/tile-layer-16.svg?raw';
+import FeatureLayerIcon from '$lib/Assets/feature-layer-16.svg?raw';
+import TileLayerIcon from '$lib/Assets/tile-layer-16.svg?raw';
 import { TreeviewNodeTypology } from '$lib/Types/treeview';
 import { ChartCandlestick, Folder, FolderOpen, Grid2x2PlusIcon } from '@lucide/svelte';
 import type { Component } from 'svelte';
