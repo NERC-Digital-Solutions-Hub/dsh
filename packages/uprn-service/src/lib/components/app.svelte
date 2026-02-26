eee93bd0d422eda129909397ae450c256091c3c9
pigeonwatcher <benjamincurry777@gmail.com>
2026-02-26 09:34:25 +0000

    docs: updated documentation for app config hook

--- packages/uprn-service/src/lib/components/app.svelte
+++ packages/uprn-service/src/lib/components/app.svelte
@@ -21,8 +21,8 @@
 	import UprnTabBar from '$lib/components/uprn-tab-bar/uprn-tab-bar.svelte';
 	import { updateSelection } from '$lib/db';
 	import { setItemInfoDialogEvents } from '$lib/Events/ItemInfoDialogEvents';
+	import { useFetchAppConfig } from '$lib/Hooks/_UseFetchAppConfig.svelte';
 	import { useAiChatbotHealth } from '$lib/Hooks/UseAiChatbotHealth.svelte';
-	import { useFetchAppConfig } from '$lib/Hooks/UseFetchAppConfig.svelte';
 	import { useLoadSelectionsFromIndexDb } from '$lib/Hooks/UseLoadSelectionsFromIndexDb.svelte';
 	import { useUprnDownloadHealth } from '$lib/Hooks/UseUprnDownloadHealth.svelte';
 	import { SelectionState } from '$lib/Models/Treeview/SelectionState';
@@ -104,7 +104,7 @@
 	const areaSelectionStore: AreaSelectionStore = new AreaSelectionStore();
 	const downloadsStore: DownloadsStore = new DownloadsStore();
 
-	/** The app configuration hook. */
+	/** Hook for fetching the app configuration. */
 	const appConfig = useFetchAppConfig();
 
 	/** State to track whether initial selections have been loaded from the database. */
