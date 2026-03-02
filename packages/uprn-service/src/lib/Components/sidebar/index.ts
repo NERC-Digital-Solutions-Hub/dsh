3b9f7d161d8f127e7bce0d7f268b1cf2e7b455a3
pigeonwatcher <benjamincurry777@gmail.com>
2026-03-02 10:52:50 +0000

    chore: updated sidebar index paths

--- packages/uprn-service/src/lib/Components/sidebar/index.ts
+++ packages/uprn-service/src/lib/Components/sidebar/index.ts
@@ -7,7 +7,7 @@
 
 import SidebarPosition from '$lib/Components/Sidebar/SidebarPosition';
 
-export { SidebarPosition } from './SidebarPosition;
+export { SidebarPosition } from './SidebarPosition';
 export { default as Sidebar } from './Sidebar.svelte';
 export { default as Root } from './SidebarRoot.svelte';
 export type PositionType = (typeof SidebarPosition)[keyof typeof SidebarPosition];
