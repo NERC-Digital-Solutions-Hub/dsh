680f170c53fa3818f843af253a05954bcb379a9d
pigeonwatcher <benjamincurry777@gmail.com>
2026-03-11 14:18:57 +0000

    refactor: added tool tips to buttons in xml renderer

--- packages/uprn-service/src/lib/Components/ItemInfoDialog/Renderers/XmlRenderer.svelte
+++ packages/uprn-service/src/lib/Components/ItemInfoDialog/Renderers/XmlRenderer.svelte
@@ -6,6 +6,7 @@
 	import XmlTree from '$lib/Components/XmlTreeview/XmlTreeview.svelte';
 	import { ArrowDownToLine } from '@lucide/svelte';
 	import ScrollArea from '$lib/Components/shadcn/scroll-area/scroll-area.svelte';
+	import * as Tooltip from '$lib/Components/shadcn/tooltip/index.js';
 
 	type Props = {
 		content: Extract<MetadataResolvedContent, { type: 'xml' }>;
@@ -28,21 +29,35 @@
 </script>
 
 <Card.Root class="w-full h-full gap-0 py-0">
-	<Card.Content class="relative h-full overflow-hidden p-4">
+	<Card.Content class="relative h-full overflow-hidden py-4 px-0">
 		<div class="absolute top-4 right-4 z-10 flex gap-2 opacity-60">
-			<CopyToClipboardButton value={content.text} variant="outline" />
+			<Tooltip.Provider disableHoverableContent>
+				<Tooltip.Root>
+					<Tooltip.Trigger class="cursor-pointer">
+						<CopyToClipboardButton value={content.text} title={''} variant="outline" />
+					</Tooltip.Trigger>
+					<Tooltip.Content side="top">Copy</Tooltip.Content>
+				</Tooltip.Root>
+			</Tooltip.Provider>
 
-			<Button
-				variant="outline"
-				size="sm"
-				disabled={!content.text}
-				onclick={() => downloadXml(content.text)}
-			>
-				<ArrowDownToLine />
-			</Button>
+			<Tooltip.Provider disableHoverableContent>
+				<Tooltip.Root>
+					<Tooltip.Trigger class="cursor-pointer">
+						<Button
+							variant="outline"
+							size="sm"
+							disabled={!content.text}
+							onclick={() => downloadXml(content.text)}
+						>
+							<ArrowDownToLine />
+						</Button>
+					</Tooltip.Trigger>
+					<Tooltip.Content side="top">Download</Tooltip.Content>
+				</Tooltip.Root>
+			</Tooltip.Provider>
 		</div>
 
-		<ScrollArea class="h-full w-full">
+		<ScrollArea class="h-full w-full px-3">
 			<XmlTree xmlText={content.text} expandAll={true} />
 		</ScrollArea>
 	</Card.Content>
