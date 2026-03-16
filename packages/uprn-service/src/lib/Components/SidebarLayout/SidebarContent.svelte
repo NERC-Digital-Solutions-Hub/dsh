5c6829e6e4633903c323f7480b1dc231f82a390a
pigeonwatcher <benjamincurry777@gmail.com>
2026-03-16 14:25:28 +0000

    fix: updated scroll area z to be on top of components in tab content

--- packages/uprn-service/src/lib/Components/SidebarLayout/SidebarContent.svelte
+++ packages/uprn-service/src/lib/Components/SidebarLayout/SidebarContent.svelte
@@ -13,7 +13,7 @@
 
 <!-- Scrollable content section -->
 <div class={cn('w-full flex-1 overflow-hidden pr-1.5 pl-1.5', className)}>
-	<ScrollArea class="h-full w-full" type="always">
+	<ScrollArea class="h-full w-full" type="always" scrollbarYClasses="z-50">
 		<div class="min-w-0 space-y-2">
 			{#if children}
 				{@render children()}
