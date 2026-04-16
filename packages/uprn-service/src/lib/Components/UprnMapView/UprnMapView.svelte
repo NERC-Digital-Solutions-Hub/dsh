fd8361e1d793f7c536194ef062c343a689ff6fd4
pigeonwatcher <benjamincurry777@gmail.com>
2026-04-16 16:46:14 +0100

    fix: added missing params

--- packages/uprn-service/src/lib/Components/UprnMapView/UprnMapView.svelte
+++ packages/uprn-service/src/lib/Components/UprnMapView/UprnMapView.svelte
@@ -19,6 +19,7 @@
 	import * as identify from '@arcgis/core/rest/identify.js';
 	import IdentifyParameters from '@arcgis/core/rest/support/IdentifyParameters.js';
 	import * as reactiveUtils from '@arcgis/core/core/reactiveUtils.js';
+	import { table } from 'console';
 
 	/**
 	 * Props accepted by the map view component.
@@ -146,7 +147,7 @@
 			await areaSelectionInteractionStore.refreshLayerView();
 			await areaSelectionInteractionStore.refreshAreas();
 
-			applyTabInteractionMode();
+			applyTabInteractionMode(currentTab, interactableLayers);
 			console.log('[uprn-map-view] MapView updated with new webMap');
 		} catch (error) {
 			console.error('Error updating MapView with new webMap:', error);
