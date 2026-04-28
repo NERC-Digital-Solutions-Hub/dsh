a195ed1d17372cb4e4aac4242e22a80893f21988
pigeonwatcher <benjamincurry777@gmail.com>
2026-04-28 17:05:25 +0100

    refactor: uprn page description added

--- hub/src/routes/apps/uprn/+page.ts
+++ hub/src/routes/apps/uprn/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
 	return {
-		title: 'UPRN Service'
+		title: 'UPRN Service',
+		description: 'Select areas, choose datasets, export UPRN results, and download files.'
 	};
 };
