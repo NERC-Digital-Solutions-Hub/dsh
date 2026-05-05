e09009291bf6bc31cd740bf1ae50e81edf4306b3
pigeonwatcher <benjamincurry777@gmail.com>
2026-05-05 14:05:10 +0100

    chore: commented out unused code

--- packages/uprn-service/src/lib/Services/config-api/IConfigurationFetcher.ts
+++ packages/uprn-service/src/lib/Services/config-api/IConfigurationFetcher.ts
@@ -1,9 +1,9 @@
-/**
- * Generic interface for fetching configuration data.
- */
-export interface IConfigurationFetcher<T> {
-	/**
-	 * Fetches the configuration item.
-	 */
-	fetch(): Promise<T>;
-}
+// /**
+//  * Generic interface for fetching configuration data.
+//  */
+// export interface IConfigurationFetcher<T> {
+// 	/**
+// 	 * Fetches the configuration item.
+// 	 */
+// 	fetch(): Promise<T>;
+// }
